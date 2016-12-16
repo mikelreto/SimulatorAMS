a54c3f0278befd06ab58478c31c4dc0e155689d4
aalzelai <andoni.alcelay@alumni.mondragon.edu>
2016-12-16 11:29:12 +0100

    Solve compilation error

--- src/main/java/domain/model/Flight.java
+++ src/main/java/domain/model/Flight.java
@@ -162,4 +162,5 @@ public class Flight implements Serializable {
 		return ticket;
 	}
 
+
 }
--- src/main/java/domain/monitor/Monitor.java
+++ src/main/java/domain/monitor/Monitor.java
@@ -1,7 +1,8 @@
 package domain.monitor;
 
 import domain.model.Flight;
-import domain.model.Line;
+import domain.model.Lane;
+
 
 /**
  * The monitor class.
@@ -22,37 +23,37 @@ public class Monitor {
      * @param vuelo is the flight that needs to see the lines.
      * @throws InterruptedException if there is any problem.
      */
-    public synchronized void enterPista(final Line currentLine,
-                                        final Line nextLine,
+    public synchronized void enterPista(final Lane currentLine,
+                                        final Lane nextLine,
                                         final Flight vuelo)
                                         throws InterruptedException {
-         while (nextLine.getTaken()) {
+         while (!nextLine.getTaken().isEmpty()) {
           // if () { Dentro de la zona de espera
-               vuelo.getAirplane().setPosX(vuelo.getAirplane().getPosX()
-                                           + currentLine.getMoveX());
-               vuelo.getAirplane().setPosY(vuelo.getAirplane().getPosY()
-                                           + currentLine.getMoveY());
+               vuelo.getPlane().setPosX(vuelo.getPlane().getPosX()
+                                           /*+ currentLine.getPosX()*/);
+               vuelo.getPlane().setPosY(vuelo.getPlane().getPosY()
+                                           /*+ currentLine.getMoveY()*/);
           //  } else {
                wait();
          //}
          }
          // while () { Fuera de la zona de espera
                move(currentLine, vuelo);
-               vuelo.getAirplane().setPosX(vuelo.getAirplane().getPosX()
-                 + currentLine.getMoveX());
-               vuelo.getAirplane().setPosY(vuelo.getAirplane().getPosY()
-                 + currentLine.getMoveY());
+               vuelo.getPlane().setPosX(vuelo.getPlane().getPosX()
+                 /*+ currentLine.getMoveX()*/);
+               vuelo.getPlane().setPosY(vuelo.getPlane().getPosY()
+                 /*+ currentLine.getMoveY()*/);
    try {
       Thread.sleep(ONESECOND);
     } catch (InterruptedException ex) {
            Thread.currentThread().interrupt();
     }
          //}
-   nextLine.setTaken(true);
+   nextLine.setTaken("Y");
 
    //vuelo.getAirplane().setPosX(nextLine.getInitialPosX());
    //cuelo.getAirplane().setPosY(nextLine.getInitialPosY());
-   currentLine.setTaken(false);
+   currentLine.setTaken("N");
    notifyAll();
     }
 
@@ -61,12 +62,12 @@ public class Monitor {
      * @param currentLine is the current line of the airplane
      * @param vuelo is the flight of the airplane
      */
-    private void move(final Line currentLine,
+    private void move(final Lane currentLine,
                       final Flight vuelo) {
-        vuelo.getAirplane().setPosX(vuelo.getAirplane().getPosX()
-                                    + currentLine.getMoveX());
-        vuelo.getAirplane().setPosY(vuelo.getAirplane().getPosY()
-                                    + currentLine.getMoveY());
+        vuelo.getPlane().setPosX(vuelo.getPlane().getPosX()
+                                    /*+ currentLine.getMoveX()*/);
+        vuelo.getPlane().setPosY(vuelo.getPlane().getPosY()
+                                    /*+ currentLine.getMoveY()*/);
     }
 
 }
